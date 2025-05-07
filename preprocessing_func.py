cab1892533e17cb8365fdbb01142617e90a238d8
muhammadnandaaf <muhammadnandaaf@gmail.com>
2025-05-08 06:55:51 +0800

    variabel sequence

--- preprocessing_func.py
+++ preprocessing_func.py
@@ -69,28 +69,29 @@ def data_preprocessing(data):
     df = pd.DataFrame()
 
     # Encoder
+    df["Marital_status"] = encoder_Marital_status.transform(data["Marital_status"])
     df["Application_mode"] = encoder_Application_mode.transform(data["Application_mode"])
     df["Course"] = encoder_Course.transform(data["Course"])
     df["Daytime_evening_attendance"] = encoder_Daytime_evening_attendance.transform(data["Daytime_evening_attendance"])
-    df["Debtor"] = encoder_Debtor.transform(data["Debtor"])
+    df["Previous_qualification"] = encoder_Previous_qualification.transform(data["Previous_qualification"])
+    df["Nacionality"] = encoder_Nacionality.transform(data["Nacionality"])
+    df["Mothers_occupation"] = encoder_Mothers_occupation.transform(data["Mothers_occupation"])
+    df["Fathers_qualification"] = encoder_Fathers_qualification.transform(data["Fathers_qualification"])
+    df["Mothers_qualification"] = encoder_Mothers_qualification.transform(data["Mothers_qualification"])
+    df["Fathers_occupation"] = encoder_Fathers_occupation.transform(data["Fathers_occupation"])
     df["Displaced"] = encoder_Displaced.transform(data["Displaced"])
     df["Educational_special_needs"] = encoder_Educational_special_needs.transform(data["Educational_special_needs"])
-    df["Fathers_occupation"] = encoder_Fathers_occupation.transform(data["Fathers_occupation"])
-    df["Fathers_qualification"] = encoder_Fathers_qualification.transform(data["Fathers_qualification"])
+    df["Debtor"] = encoder_Debtor.transform(data["Debtor"])
+    df["Tuition_fees_up_to_date"] = encoder_Tuition_fees_up_to_date.transform(data["Tuition_fees_up_to_date"])
     df["Gender"] = encoder_Gender.transform(data["Gender"])
+    df["Scholarship_holder"] = encoder_Scholarship_holder.transform(data["Scholarship_holder"])
     df["International"] = encoder_International.transform(data["International"])
-    df["Marital_status"] = encoder_Marital_status.transform(data["Marital_status"])
-    df["Mothers_occupation"] = encoder_Mothers_occupation.transform(data["Mothers_occupation"])
-    df["Mothers_qualification"] = encoder_Mothers_qualification.transform(data["Mothers_qualification"])
 
     # Scaler
+    df["Application_order"] = scaler_Application_order.transform(np.asarray(data["Application_order"]).reshape(-1, 1))
+    df["Previous_qualification_grade"] = scaler_Previous_qualification_grade.transform(np.asarray(data["Previous_qualification_grade"]).reshape(-1, 1))
     df["Admission_grade"] = scaler_Admission_grade.transform(np.asarray(data["Admission_grade"]).reshape(-1, 1))
     df["Age_at_enrollment"] = scaler_Age_at_enrollment.transform(np.asarray(data["Age_at_enrollment"]).reshape(-1, 1))
-    df["Application_order"] = scaler_Application_order.transform(np.asarray(data["Application_order"]).reshape(-1, 1))
-    df["GDP"] = scaler_GDP.transform(np.asarray(data["GDP"]).reshape(-1, 1))
-    df["Inflation_rate"] = scaler_Inflation_rate.transform(np.asarray(data["Inflation_rate"]).reshape(-1, 1))
-    df["Previous_qualification_grade"] = scaler_Previous_qualification_grade.transform(np.asarray(data["Previous_qualification_grade"]).reshape(-1, 1))
-    df["Unemployment_rate"] = scaler_Unemployment_rate.transform(np.asarray(data["Unemployment_rate"]).reshape(-1, 1))
 
     # PCA1
     data["Curricular_units_1st_sem_credited"] = scaler_Curricular_units_1st_sem_approved.transform(np.asarray(data["Curricular_units_1st_sem_approved"]).reshape(-1, 1))[0]
@@ -111,6 +112,10 @@ def data_preprocessing(data):
     data["Curricular_units_2nd_sem_without_evaluations"] = scaler_Curricular_units_2nd_sem_without_evaluations.transform(np.asarray(data["Curricular_units_2nd_sem_without_evaluations"]).reshape(-1, 1))[0]
 
     df[["pc1_1", "pc1_2", "pc1_3", "pc1_4"]] = pca_2.transform(data[pca_academic_semester_2])
+
+    df["Unemployment_rate"] = scaler_Unemployment_rate.transform(np.asarray(data["Unemployment_rate"]).reshape(-1, 1))
+    df["Inflation_rate"] = scaler_Inflation_rate.transform(np.asarray(data["Inflation_rate"]).reshape(-1, 1))
+    df["GDP"] = scaler_GDP.transform(np.asarray(data["GDP"]).reshape(-1, 1))
 
     return df
 
